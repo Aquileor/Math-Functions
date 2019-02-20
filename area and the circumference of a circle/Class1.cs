37c416e04ee87ec70b9d7d41f6ee49afd0b4f3cf
Aquileo Reyes <aquileoreyes@gmail.com>
2019-02-20 08:06:55 -0500

    Add project files.

---
+++ area and the circumference of a circle/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace area_and_the_circumference_of_a_circle
+{
+    public class Class1
+    {
+    }
+}
